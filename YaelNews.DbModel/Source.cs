5403e6be997e718f579407198560728ebc9d3938
Yehuda Ben Yehuda <ybenyehuda@crossriverbank.com>
2022-07-17 02:39:09 +0300

    initial infrastructure

---
+++ YaelNews.DbModel/Source.cs
@@ -0,0 +1,17 @@
+﻿namespace YaelNews.DbModel
+{
+    public partial class Source
+    {
+        public Source()
+        {
+            Items = new List<Item>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string BaseUrl { get; set; } = null!;
+        public string IconUrl { get; set; } = null!;
+
+        public virtual List<Item> Items { get; set; }
+    }
+}
