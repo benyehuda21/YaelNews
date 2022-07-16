3319c73a75af3ac017a44b23ff70e79186e68000
Yehuda Ben Yehuda <ybenyehuda@crossriverbank.com>
2022-07-17 01:44:40 +0300

    initial files

---
+++ YaelMews.Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YaelMews.Model
+{
+    public class Item
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public Guid SourceID { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        public string? Writer { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
