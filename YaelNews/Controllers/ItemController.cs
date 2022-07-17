905254949c978c1fcb85f90232f68d9125041360
Yehuda Ben Yehuda <ybenyehuda@crossriverbank.com>
2022-07-18 01:05:18 +0300

    rest api start

---
+++ YaelNews/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YaelMews.Model;
+
+namespace YaelNews.Controllers
+{
+    // https://www.yaelnews.com/api/items
+    // rest api
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        //public ItemController()
+        //{
+
+        //}
+
+        //[HttpGet]
+        //public async Task<List<Item>> Get()
+        //{
+        //    // returm
+        //}
+
+        //[HttpPut]
+        //public async Task Update(Item item)
+        //{
+
+        //}
+
+        //[HttpPost]
+        //public async Task AddNewItem(Item item)
+        //{
+
+        //}
+
+    }
+}
--- YaelNews/Program.cs
+++ YaelNews/Program.cs
@@ -1,10 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -14,12 +9,6 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapRazorPages();
-
 app.Run();
